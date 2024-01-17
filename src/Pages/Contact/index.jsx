d24747ec23f9e281740d5011d09bf94e19f4d638
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-01-17 21:45:59 +0500

    Some Changes

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -3,19 +3,20 @@ import { Navbar } from "../../Components";
 
 const Contact = () => {
   return (
-    <div id="contact" className="w-full pt-10 bg-[#5271FF] ">
+    <div id="contact" className="w-full  ">
       {/* <Navbar /> */}
-      <div className="w-full mx-auto">
-        <div className="text-center h-[200px] flex flex-col justify-center bg-white rounded-b-[130px]">
-          <h1 className="sm:text-3xl text-2xl text-center font-bold text-[#5271FF]">Contact Us</h1>
+      <div className="w-full mx-auto py-4">
+        <div className="text-center p-5 flex flex-col justify-center bg-[#5271FF] rounded-t-[130px]">
+          <h1 className="sm:text-3xl text-2xl text-center font-bold text-white">Contact Us</h1>
           <p className="font-bold">
-            Phone: <span className="text-[#5271FF]">03198187639</span>
+            Phone: <span className="text-white">03198187639</span>
           </p>
           <p className="font-bold">
-            Email: <span className="text-[#5271FF]">ayansheikh1200@gmail.com</span>
+            Email: <span className="text-white">ayansheikh1200@gmail.com</span>
           </p>
         </div>
-        <div className="w-[90%] mx-auto flex flex-col md:flex-row  mt-[40px] items-center ">
+        <div className="w-full bg-[#5271FF] ">
+        <div className="w-[90%] mx-auto flex flex-col md:flex-row items-center ">
           <div className="w-[90%] flex flex-col gap-5 p-2">
             <input
               className="p-3 text-lg rounded-lg focus:outline-[#5271FF]"
@@ -49,8 +50,10 @@ const Contact = () => {
             />
           </div>
         </div>
+        </div>
+        
         <div className="w-full flex justify-center mt-10 ">
-          <button className="p-2 bg-white rounded-md hover:bg-gray-300">Submit</button>
+          <button className="p-2 bg-[#5271FF] rounded-md hover:bg-[#2c53ff] text-white font-bold">Submit</button>
         </div>
       </div>
     </div>
