cf3395f9f5228ecb2642e40f78c5cef14b0a0673
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-03-19 03:21:17 +0500

    firebase Conneted

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,31 +1,186 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 const DashBoard = () => {
-    const loggedin = localStorage.getItem("both")
-    console.log(loggedin);
-  return !loggedin ? ( <Navigate to='/'/> ) : ( <>
-    <div className='w-full h-dvh flex flex-col justify-center'>
-        <div className='w-[90%] mx-auto bg-[#5271FF] py-10  rounded-2xl'>
-            <h1 className='text-4xl text-center text-white font-semibold'>Add Project</h1>
-        <form action="" className=' w-[500px] flex flex-col gap-2 bg-white mx-auto p-4 py-14 rounded-3xl mt-4'>
-                <label htmlFor="">Title:</label>
-                <input className='w-full p-2 text-black rounded-lg border-2 border-[#5271FF]' type="text" name="" id="" placeholder='Enter Email' />
-                <label htmlFor="">Decription:</label>
-                <textarea className='w-full p-2 text-black rounded-lg border-2 border-[#5271FF]'  name="" id="" placeholder='Enter Password' /> 
-                <label htmlFor="">Live Link:</label>
-                <input className='w-full p-2 text-black rounded-lg border-2 border-[#5271FF]' type="text" name="" id="" placeholder='Enter Email' />
-                <label htmlFor="">Repo Link:</label>
-                <input className='w-full p-2 text-black rounded-lg border-2 border-[#5271FF]' type="text" name="" id="" placeholder='Enter Email' />
-                <label htmlFor="">Image:</label>
-                <input className='w-full p-2 text-black rounded-lg border-2 border-[#5271FF]' type="file" name="" id="" placeholder='Enter Email' />
-                <button className='w-full bg-[#5271FF] text-white font-bold p-2 mt-3 rounded-lg'>Login</button>
+  const [image, setImage] = useState();
+  const [title, setTitle] = useState();
+  const [desc, setDesc] = useState();
+  const [liveLink, setLiveLink] = useState();
+  const [repoLink, setRepoLink] = useState();
+  const [category, setCategory] = useState();
+  const loggedin = localStorage.getItem("/AT/");
 
-            </form>
+  const userId = '1eFEWPdNgFSmRKkcWWddASRHs942'
+
+  const imageUpload = (e) => {
+    e.preventDefault();
+    const storage = getStorage();
+
+    if(userId != loggedin){
+      return
+    }
+    if(!repoLink && !title && !desc && !category){
+      return alert("Enter Req Feilds")
+    }
+
+    // Create the file metadata
+    /** @type {any} */
+    const metadata = {
+      contentType: "image/jpeg",
+    };
+
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    const storageRef = ref(storage, "images/" + image.name);
+    const uploadTask = uploadBytesResumable(storageRef, image, metadata);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            // User doesn't have permission to access the object
+            break;
+          case "storage/canceled":
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case "storage/unknown":
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          
+          console.log("File available at", downloadURL);
+
+
+          const docRef = await addDoc(collection(db, "Projects"), {
+            title,
+            desc,
+            liveLink,
+            repoLink,
+            ImageUrl : downloadURL,
+            category
+
+          });
+          console.log("Document written with ID: ", docRef.id);
+
+
+        });
+      }
+    );
+  };
+
+  
+  // console.log(loggedin);
+  return !loggedin ? (
+    <Navigate to="/" />
+  ) : (
+    <>
+      <div className="w-full h-dvh flex flex-col justify-center">
+        <div className="w-[90%] mx-auto bg-gradient-to-bl from-[#5271FF] via-purple-500 to-pink-500 py-10  rounded-2xl">
+          <h1 className="text-4xl text-center text-white font-semibold">
+            Add Project
+          </h1>
+          <form
+            // action=""
+            onSubmit={(e)=>imageUpload(e)}
+            className=" w-[500px] flex flex-col gap-2 bg-white mx-auto p-4 py-14 rounded-3xl mt-4"
+          >
+            <label htmlFor="">Title:</label>
+            <input
+              className="w-full p-2 text-black rounded-lg border-2 border-[#5271FF]"
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter Title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <label htmlFor="">Decription:</label>
+            <textarea
+              className="w-full p-2 text-black rounded-lg border-2 border-[#5271FF]"
+              name=""
+              id=""
+              placeholder="Enter Description"
+              onChange={(e) => setDesc(e.target.value)}
+            />
+            <label htmlFor="">Live Link:</label>
+            <input
+              className="w-full p-2 text-black rounded-lg border-2 border-[#5271FF]"
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter Live Link"
+              onChange={(e) => setLiveLink(e.target.value)}
+            />
+            <label htmlFor="">Repo Link:</label>
+            <input
+              className="w-full p-2 text-black rounded-lg border-2 border-[#5271FF]"
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter Repo Link"
+              onChange={(e) => setRepoLink(e.target.value)}
+            />
+            <label htmlFor="">Category:</label>
+            <select
+              onChange={(e) => setCategory(e.target.value)}
+              className="w-full p-2 text-black rounded-lg border-2 border-[#5271FF]"
+            >
+              <option className="w-full" value="None">
+                None
+              </option>
+              <option className="w-full" value="App">
+                App
+              </option>
+              <option className="w-full" value="Website">
+                Website
+              </option>
+              <option className="w-full" value="Backend">
+                Backend
+              </option>
+            </select>
+            <label htmlFor="">Image:</label>
+            <input
+              className="w-full p-2 text-black rounded-lg border-2 border-[#5271FF]"
+              type="file"
+              name=""
+              id=""
+              placeholder="Enter Email"
+              onChange={(e) => setImage(e.target.files[0])}
+            />
+            <button type="submit" className="w-full bg-gradient-to-r from-[#5271FF] via-purple-500 to-pink-500 text-white font-bold p-2 mt-3 rounded-lg">
+              Add
+            </button>
+          </form>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DashBoard
+export default DashBoard;
--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,26 +1,54 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import {getAuth, signInWithEmailAndPassword} from '../../../firebase'
 
 const Login = () => {
-  const loggedin = localStorage.getItem("/AT/");
+  const [loggedin, setLoggedin ] = useState("")
+  const [email, setEmail ] = useState("")
+  const [password, setPassword ] = useState("")
+  // const Navigation = useNavigate("")
 
-  const Signing = () => {
-    // const auth = getAuth();
-    // createUserWithEmailAndPassword(auth, email, password)
-    //   .then((userCredential) => {
-    //     // Signed up
-    //     const user = userCredential.user;
-    //     // ...
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     // ..
-    //   });
+  useEffect(()=>{
+
+    const aloggedin = localStorage.getItem("/AT/");
+    setLoggedin(aloggedin)
+
+
+  }, [])
+
+
+  // const loggedin = localStorage.getItem("/AT/");
+  const auth = getAuth();
+
+  // console.log(auth);
+
+  const Signing = async (e) => {
+    // console.log(e);
+
+    e.preventDefault();
+
+    if(!email && !password){
+      return alert("Enter Please Login Info")
+    }
+    
+    await signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user.uid);
+
+      localStorage.setItem("/AT/",user.uid)
+      // Navigation("dashboard")
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
   };
 
   return loggedin ? (
-    <Navigate to="/dashboard" />
+    <Navigate to="/dashboard" replace={true} />
   ) : (
     <>
      <div className="w-full flex flex-col justify-center items-center">
@@ -29,35 +57,35 @@ const Login = () => {
 
       <div className="w-[400px] h-[600px] mt-14 ">
 
-      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
  <h1 className="text-center text-4xl font-bold">LOGO</h1>
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
   </div>
 
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6" onSubmit={(e)=>Signing(e)}>
       <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div className="mt-2">
+          <input onChange={(e)=>setEmail(e.target.value)} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
 
       <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-          <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+        <div className="flex items-center justify-between">
+          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          <div className="text-sm">
+            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
           </div>
         </div>
-        <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        <div className="mt-2">
+          <input onChange={(e)=>setPassword(e.target.value)} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
 
       <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-gradient-to-bl from-[#5271FF] via-purple-500 to-pink-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+        <button type="submit" className="flex w-full justify-center rounded-md bg-gradient-to-bl from-[#5271FF] via-purple-500 to-pink-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
       </div>
     </form>
 
